aa743784d48c1262ca5a76449973a6e787675b04
madhuri <madhurikkansara@gmail.com>
2021-10-15 21:27:08 -0400

    first commit

---
+++ src/test/java/mavenProject/mavenJava/AppiumTest.java
@@ -0,0 +1,19 @@
+package mavenProject.mavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+
+}
---
+++ src/test/java/mavenProject/mavenJava/RestAPITest.java
@@ -0,0 +1,19 @@
+package mavenProject.mavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+
+}
